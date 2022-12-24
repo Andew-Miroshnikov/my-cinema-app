90cb79bbf6258f9ea5a16084216f00c2cb8d366a
Andrew Miroshnikov <giggle2594@gmail.com>
2022-12-24 22:59:11 +0200

    Add remde file

---
+++ src/main/java/cinema/service/impl/UserServiceImpl.java
@@ -0,0 +1,43 @@
+package cinema.service.impl;
+
+import cinema.dao.UserDao;
+import cinema.model.User;
+import cinema.service.UserService;
+import java.util.Optional;
+import javax.persistence.EntityNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final PasswordEncoder encoder;
+    private final UserDao userDao;
+
+    public UserServiceImpl(PasswordEncoder encoder, UserDao userDao) {
+        this.encoder = encoder;
+        this.userDao = userDao;
+    }
+
+    @Override
+    public User add(User user) {
+        user.setPassword(encoder.encode(user.getPassword()));
+        return userDao.add(user);
+    }
+
+    @Override
+    public User get(Long id) {
+        return userDao.get(id).orElseThrow(
+                () -> new RuntimeException("User with id " + id + " not found"));
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        return userDao.findByEmail(email);
+    }
+
+    @Override
+    public User getByName(String email) {
+        return userDao.findByEmail(email).orElseThrow(() ->
+                new EntityNotFoundException("Can't get user by email" + email));
+    }
+}
